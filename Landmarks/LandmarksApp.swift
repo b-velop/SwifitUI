80001b9dc77e4e7c8410df8ea1df8dfb9efec8bd
Marcel Benders <marcel.benders@b-velop.com>
2022-12-04 14:47:04 +0100

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Marcel Benders on 04.12.22.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
