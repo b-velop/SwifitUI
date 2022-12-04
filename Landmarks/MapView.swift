9c9790e4f4fab9ac897320af678186293a73def2
Marcel Benders <marcel.benders@b-velop.com>
2022-12-04 20:04:30 +0100

    End of Content Part

--- Landmarks/MapView.swift
+++ Landmarks/MapView.swift
@@ -14,7 +14,7 @@ struct MapView: View {
      */
     @State private var region = MKCoordinateRegion(
         center: CLLocationCoordinate2D(latitude: 34.011_286, longitude: -116.166_868),
-        span: MKCoordinateSpan(latitudeDelta: 2, longitudeDelta: 2))
+        span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1))
     
     var body: some View {
         /*
