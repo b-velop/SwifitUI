ae93750b1970f4523bfcce22a017712c2d4e9050
Marcel Benders <marcel.benders@b-velop.com>
2022-12-16 08:23:04 +0100

    Section 4

--- Landmarks/Views/LandmarkList.swift
+++ Landmarks/Views/LandmarkList.swift
@@ -8,13 +8,29 @@
 import SwiftUI
 
 struct LandmarkList: View {
-    var body: some View {
+    @State private var showFaforitesOnly = false
+    
+    var filteredLandmarks: [Landmark] {
+        landmarks.filter{landmark in
+            (!showFaforitesOnly || landmark.isFavorite)
+        }
+    }
+    
+     var body: some View {
         NavigationView{
-            List(landmarks){ landmark in
-                NavigationLink{
-                    LandmarkDetail(landmark: landmark)
-                }label: {
-                    LandmarkRow(landmark: landmark)
+            List{
+                /**
+                 You use the $ prefix to access a binding to a state variable, or one of its properties.
+                 */
+                Toggle(isOn: $showFaforitesOnly) {
+                    Text("Faforites Only")
+                }
+                ForEach(filteredLandmarks){landmark in
+                    NavigationLink{
+                        LandmarkDetail(landmark: landmark)
+                    }label: {
+                        LandmarkRow(landmark: landmark)
+                    }
                 }
             }
             .navigationTitle("Landmarks")
@@ -24,11 +40,11 @@ struct LandmarkList: View {
 
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
-        ForEach(["iPhone SE (2nd generation)", "iPhone XS Max"], id: \.self) { deviceName in
+     //   ForEach(["iPhone SE (2nd generation)", "iPhone XS Max"], id: \.self) { deviceName in
             LandmarkList()
-                .previewDevice(PreviewDevice(rawValue: deviceName))
-                .previewDisplayName(deviceName)
-        }
+           //     .previewDevice(PreviewDevice(rawValue: deviceName))
+            //    .previewDisplayName(deviceName)
+       // }
     }
 
 }
